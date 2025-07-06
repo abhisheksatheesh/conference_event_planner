b4276c5d446b8a77aab8be2cdabf5eb3cbab4943
abhisheksatheesh <abhisheksatheesh92@gmail.com>
2025-07-06 09:53:25 -0400

    added shopping details and added total cost on various sections

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSlice'
+import mealsReducers from './mealsSlice'
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av:avReducer,
+    meals:mealsReducers,
   },
 });
